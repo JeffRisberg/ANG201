b9ff0644182bfc260df463aa8ffcd284db8c21f1
JeffRisberg <jrisberg@aol.com>
2016-01-31 21:26:33 -0800

    Updated to include examples from 1/31

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,6 @@
 // Ref:
 // https://angular.io/docs/ts/latest/quickstart.html
 
-// In my Angular2 app, why is this file called app.component.ts?
-
 // In Angular2, what does import do?
 // In Angular2, what is Component?
 // In Angular2, what is 'angular2/core'?
@@ -18,7 +16,7 @@ import {Component} from 'angular2/core';
 
 // In Angular2, what does export do?
 export class AppComponent {
-    public title = 'Hello Today is January 31';
+    public title = 'Hello, Today is January 31';
     public namesOfBadGuys = ["Darth Vader", "Lex Luthor", "Sauron", "The Joker"];
 }
 // In Angular2, what does class do?
