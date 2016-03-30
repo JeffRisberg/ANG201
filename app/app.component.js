eccedeb31d4516009aa6c87b07f0d5febda7f962
JeffRisberg <jrisberg@aol.com>
2016-03-29 22:34:31 -0700

    Updated to include latest version of React and tools

--- app/app.component.js
+++ app/app.component.js
@@ -1,6 +1,8 @@
 // Ref:
 // https://angular.io/docs/ts/latest/quickstart.html
-System.register(['angular2/core'], function(exports_1) {
+System.register(['angular2/core'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -18,26 +20,22 @@ System.register(['angular2/core'], function(exports_1) {
                 core_1 = core_1_1;
             }],
         execute: function() {
-            // In Angular2, when I use the @ sign, what does the @ sign do?
             AppComponent = (function () {
                 function AppComponent() {
-                    this.title = 'Hello Today is January 31';
+                    this.title = 'This is Angular 2 example 01';
                     this.namesOfBadGuys = ["Darth Vader", "Lex Luthor", "Sauron", "The Joker"];
                 }
                 AppComponent = __decorate([
                     core_1.Component({
-                        // In Angular2, what does selector property do?
                         selector: 'my-app',
                         templateUrl: 'app/app.component.html',
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
                 return AppComponent;
-            })();
+            }());
             exports_1("AppComponent", AppComponent);
         }
     }
 });
-// In Angular2, what does class do?
-// In my Angular2 app, where else do I see AppComponent?
 //# sourceMappingURL=app.component.js.map
